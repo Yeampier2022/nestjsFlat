25e3dfacd5382c0d2eb5364751490069eeb120d9
Yeampier2022 <yeampierjesusa@gmail.com>
2024-01-25 11:46:20 -0400

    feat: validacion

--- src/task/dto/task.dto.ts
+++ src/task/dto/task.dto.ts
@@ -1,12 +1,26 @@
 import { TaskStatus } from '../task.entity';
+import { IsIn, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
 
 export class createTaskDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(4)
   title: string;
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(4)
   description: string;
 }
 
 export class updateTaskDto {
+  @IsString()
+  @IsOptional()
   title?: string;
+  @IsString()
+  @IsOptional()
   description?: string;
+  @IsString()
+  @IsOptional()
+  @IsIn([TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
   status?: TaskStatus;
 }
